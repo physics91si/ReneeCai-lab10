7d9668a11e6e34d7e2ed6b05c262a6c9aa33c420
Renee Cai <reneecai@gmail.com>
2016-05-03 13:42:24 -0700

    sets.py

---
+++ sets.py
@@ -0,0 +1,17 @@
+import classes
+class Sets:
+    def __init__(self,num)
+        self.num = num
+    def __contains__(self, x)
+        if x in self:
+            print (x)
+    def __add__(self, other):
+        self.number += other.number
+        return self 
+    def 
+class remove(element):
+    def __init__(self, el):
+        self.el = el
+    def __
+
+    
